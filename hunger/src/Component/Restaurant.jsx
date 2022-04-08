18d8f27e95355611dd1d05c5bb96548953a47996
Harshal  Pagar <72097667+harshpagar@users.noreply.github.com>
2022-04-08 14:59:17 +0530

    add
    add

---
+++ hunger/src/Component/Restaurant.jsx
@@ -0,0 +1,16 @@
+import "./Restaurant.css";
+
+const Restaurant = () => {
+    return(
+        <div className="card-container">
+            <div className="card">
+                <div className="card-body">
+                    <span className="card-number card-circle subtle">1</span>
+                    <span className="card-author subtle"></span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Restaurant
