505411228be5fa13e6374e79ecb6f6dd31b97ffa
Harshal  Pagar <72097667+harshpagar@users.noreply.github.com>
2022-04-08 23:43:47 +0530

    add
    add

--- hunger/src/Component/Restaurant.jsx
+++ hunger/src/Component/Restaurant.jsx
@@ -6,8 +6,15 @@ const Restaurant = () => {
             <div className="card">
                 <div className="card-body">
                     <span className="card-number card-circle subtle">1</span>
-                    <span className="card-author subtle"></span>
+                    <span className="card-author subtle">Breakfast</span>
+                    <h2 className="card-title">Sevenin</h2>
+                    <span className="card-description subtle">
+                        Pizza,Chines.
+                    </span>
+                    <div className="card-read">Read</div>
                 </div>
+                
+                <span className="card-tag subtle ">Order Now</span>
             </div>
         </div>
     )
